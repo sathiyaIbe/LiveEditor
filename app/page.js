945c6de223837375710286943c8a59380663b7ac
sathiyaibe <sathiyaibd@gmail.com>
2024-02-09 11:31:49 +0530

    eslint disable

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {  Navbar } from '../components';
 import {  Hero, } from '../sections';
 
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable */
 import React from "react";
 import {useEffect} from "react";
 import Webcam from "react-webcam";
