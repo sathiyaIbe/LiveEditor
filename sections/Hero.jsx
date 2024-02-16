e4c655847e4dc7b9f84fdbd5ff53dce51865db13
sathiyaibe <sathiyaibd@gmail.com>
2024-02-16 10:14:28 +0530

    added the size to the mob version24

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () =>
   function functionToFocus() {
     
      document.getElementById('input').focus();
-     if(text=="Enter text"){
+     if(text==="Enter text"){
      setText("");
      }
   }
@@ -73,8 +73,7 @@ const Hero = () =>
    
   };
   const onChangeText = () => {
-setText("Enter text");
-setToggleText(!toggleText);
+setToggleText(true);
 };
 useEffect(() =>{
     
@@ -120,114 +119,60 @@ return(
   <section>
  
       <div>
-      {/* <button onClick={openImgEditor}>Open Filerobot image editor</button> */}
-      {isImgEditorShown ? (
-//         <div className="h-[700px] w-[500px]  md:w-[1300px] md:h-[700px]">
-//         <FilerobotImageEditor
-//         className="filerobot-editor"
-       
-//           source={image}
-//           onSave={(editedImageObject, designState) =>
-//             saved(editedImageObject,designState)
+      {isImgEditorShown ?
+    
+<>
+{!toggleText&&
+<div className="flex flex-col">
+<h1 className="Header mb-6 mt-6">Enter The Comments</h1>
 
-//           }
-//           onClose={closeImgEditor}
-//           annotationsCommon={{
-//             fill: '#000',
-//           }}
-//           Text={{ text: 'Enter Text' }}
-//           Rotate={{ angle: 90, componentType: 'slider' }}
-//           Crop={{
-//             presetsItems: [
-//               {
-//                 titleKey: 'classicTv',
-//                 descriptionKey: '4:3',
-//                 ratio: 4 / 3,
-//                 // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
-//               },
-//               {
-//                 titleKey: 'cinemascope',
-//                 descriptionKey: '21:9',
-//                 ratio: 21 / 9,
-//                 // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
-//               },
-//             ],
-//             presetsFolders: [
-//               {
-//                 titleKey: 'socialMedia', // will be translated into Social Media as backend contains this translation key
-//                 // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
-//                 groups: [
-//                   {
-//                     titleKey: 'facebook',
-//                     items: [
-//                       {
-//                         titleKey: 'profile',
-//                         width: 180,
-//                         height: 180,
-//                         descriptionKey: 'fbProfileSize',
-//                       },
-//                       {
-//                         titleKey: 'coverPhoto',
-//                         width: 820,
-//                         height: 312,
-//                         descriptionKey: 'fbCoverPhotoSize',
-//                       },
-//                     ],
-//                   },
-//                 ],
-//               },
-//             ],
-//           }}
-//           tabsIds={[TABS.ADJUST, TABS.ANNOTATE, TABS.WATERMARK,TABS.FILTERS,TABS.FINETUNE]} // or {['Adjust', 'Annotate', 'Watermark']}
-//           defaultTabId={TABS.ANNOTATE} // or 'Annotate'
-//           defaultToolId={TOOLS.TEXT} // or 'Text'
-//         />
-// </div>
+<textarea row="3" id="input"  className="md:max-w-[40vw] textArea max-w-[80vw] min-h-[25vh] resize-y resize-x  track-[1px]  self-center text-center  bg-transparent border  border-none text-black  rounded-lg focus:ring-none focus:border-none block w-full p-2.5 "
+ type='text' onChange={onChange} value={text}></textarea>
+ <button type="button" className="my-6 self-center  max-w-[150px]  px-8 py-3 text-lg font-bold text-yellow-500 transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" onClick={onChangeText}>Next</button>
+</div>
+}
+{toggleText&&
+
+        
 <div className="flex flex-col ">
   <div className='self-center'>
     <div className='flex  justify-end z-[100]  left-0 right-0 absolute w-[94vw] md:w-[90vw]'>
     <div className='flex justify-end   md:w-[50vw] w-[90vw]  py-11'>
 
   <div className='flex gap-6 pr-3'>
-  <button type='button' onClick={onChangeText} className='bg-zinc-950 cursor-pointer bg-opacity rounded-full p-1 w-25 md:p-3 text-white'>
+  {/* <button type='button' onClick={onChangeText} className='bg-zinc-950 cursor-pointer bg-opacity rounded-full p-1 w-25 md:p-3 text-white'>
 
 <IoText className='ext-white text-[28px] md:text-[36px] p-1 '  />
-</button>
+</button> */}
     <div className='bg-zinc-950 rounded-full p-1 md:p-3 text-white'>
-
-
     <LuDownloadCloud className='text-white text-[28px] md:text-[36px] p-1  cursor-pointer' onClick={onButtonClick}/>
      </div>
      <div className='bg-zinc-950 rounded-full p-1 md:p-3 text-white'>
-
     <AiOutlineClose className='ext-white text-[28px] md:text-[36px] p-1  cursor-pointer' onClick={closeImgEditor} />
     </div>
-  
     </div>
     </div>
-
-
     </div>
-  {/* <div className="w-[80vw] flex flex-col  ">
-</div>  */}
 
 <div ref={ref} className='flex flex-col min-h-[83vh] mx-2 justify-center aligns-center'>
 
- {toggleText&&
 
 
  <div className=' text-fix self-center pt-3 flex fex-col justify-center items-center absolute cursor-move z-[100]'>
  <Draggable
- allowAnyClick={true}
- onStart={functionToFocus }
-  className=''  >
-
+    >
+      <textarea row="3" className="text-fix text-white max-w-[80vw] min-h-[25vh] bg-transparent">
+    {text}
+    </textarea>
+{/* 
 <textarea row="3" id="input"  className="md:max-w-[40vw] max-w-[80vw] min-h-[25vh] resize-y resize-x  track-[1px]  self-center text-center  bg-transparent border  border-none text-white  rounded-lg focus:ring-none focus:border-none block w-full p-2.5 "
- type='text' onChange={onChange} value={text}></textarea>
+ type='text' onChange={onChange} value={text}></textarea> */}
   </Draggable>
  </div>
+ 
 
-}
+      
+
 <div className="flex flex-col ">
  <img src="frame.png" className='absolute self-center min-w-[90vw] min-h-[85vh] ' alt="frame"/>
  </div>
@@ -235,7 +180,9 @@ return(
 </div>
 </div>
 </div>
-      ):
+}
+</>
+      :
       <div className="flex flex-col items-center mt-11 md:pt-6 " >
   <button className=" my-6   max-w-[150px] z-10 absolute bottom-[-2%]  px-8 py-3 text-lg font-bold text-yellow-500 transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" onClick={capture}>
         Capture</button>
